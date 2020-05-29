2f080c3d7ad93a21e2f447617bf9ffa1c4c27b76
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2020-05-29 19:49:59 +0430

    Refactor action types

--- src/constants/types.js
+++ src/constants/types.js
@@ -1,14 +1,15 @@
-export const UPDATE_COLUMN = "UPDATE_COLUMN";
-export const UPDATE_COLUMN_ORDER = "UPDATE_COLUMN_ORDER";
-export const NO_CHANGE = "NO_CHANGE";
+// General
 export const INITAL_DATA = "INITAL_DATA";
-
+export const NO_CHANGE = "NO_CHANGE";
+// Task
 export const ADD_TASK = "ADD_TASK";
 export const UPDATE_TASK = "UPDATE_TASK";
 export const DELETE_TASK = "DELETE_TASK";
-
+// Column
 export const ADD_COLUMN = "ADD_COLUMN";
+export const UPDATE_COLUMN = "UPDATE_COLUMN";
 export const DELETE_COLUMN = "DELETE_COLUMN";
-
+export const UPDATE_COLUMN_ORDER = "UPDATE_COLUMN_ORDER";
+// Message
 export const RAISE_MESSAGE = "RAISE_MESSAGE";
 export const DELETE_MESSAGE = "DELETE_MESSAGE";
