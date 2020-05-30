dccad483a86a6f99400a99a6d0ba875656418a54
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2020-05-30 22:41:56 +0430

    FIX Readme bugses

--- src/components/Constructors/Column/__tests__/Column.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Column from "../Column";
-
-test("renders without no problem", () => {
-  const wrapper = render(<Column />);
-  expect(wrapper).toBeTruthy();
-});
